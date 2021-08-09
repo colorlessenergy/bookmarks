632efbd425053acda6ae393c45f9418c2aa2f522
colorlessenergy <brianmunozdev@gmail.com>
2021-08-09 08:05:46 -0700

    create import bookmarks page

---
+++ pages/settings/import-bookmarks.js
@@ -0,0 +1,14 @@
+import Nav from "../../shared/components/Nav";
+
+export default function ImportBookmarks () {
+    return (
+        <div className="container">
+            <Nav showHomeLink={ true } />
+
+            <h1 className="mb-1">
+                import bookmarks
+            </h1>
+            <button className="button button-blue">import</button>
+        </div>
+    );
+}
