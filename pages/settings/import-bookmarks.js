73d45f31c5c5d18af1d571f334281842f1986b0e
colorlessenergy <brianmunozdev@gmail.com>
2021-08-09 08:24:01 -0700

    add functionality to get all links from exported bookmarks html file

--- pages/settings/import-bookmarks.js
+++ pages/settings/import-bookmarks.js
@@ -1,6 +1,18 @@
 import Nav from "../../shared/components/Nav";
 
 export default function ImportBookmarks () {
+    const importBookmarks = (event) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const importedBookmarks = e.target.result;
+            const parser = new DOMParser();
+            const parsedHTML = parser.parseFromString(importedBookmarks, "text/html");
+            let bookmarks = parsedHTML.querySelectorAll('a');
+        }
+
+        reader.readAsText(event.target.files[0]);
+    }
+    
     return (
         <div className="container">
             <Nav showHomeLink={ true } />
@@ -17,6 +29,7 @@ export default function ImportBookmarks () {
                 type="file"
                 id="import-bookmarks"
                 accept=".html"
+                onChange={ importBookmarks }
                 className="d-none" />
         </div>
     );
