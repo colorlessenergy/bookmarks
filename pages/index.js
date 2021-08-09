b4336f43bd0e5ada7a8aab30ba6b85fb32aa9d17
colorlessenergy <brianmunozdev@gmail.com>
2021-08-09 11:40:26 -0700

    add HTML structure to modal

--- pages/index.js
+++ pages/index.js
@@ -77,9 +77,51 @@ export default function Home () {
             }) : (null) }
 
             <Modal isOpen={ isModalOpen }>
-                <h1>
-                    modal
-                </h1>
+                <form className="flex flex-direction-column align-items-start">
+                    <label
+                        htmlFor="URL"
+                        className="mb-05">
+                        URL
+                    </label>
+                    <input
+                        type="text"
+                        autoComplete={ false }
+                        id="URL"
+                        className="input input-form mb-1" />
+                    <label
+                        htmlFor="title"
+                        className="mb-05">
+                        title
+                    </label>
+                    <input
+                        type="text"
+                        autoComplete={ false }
+                        id="title"
+                        className="input input-form mb-1" />
+
+                    <label
+                        htmlFor="description"
+                        className="mb-05">
+                        description
+                    </label>
+                    <textarea
+                        type="text"
+                        autoComplete={ false }
+                        id="description"
+                        className="input input-form textarea mb-2 pt-1"></textarea>
+                    
+                    <div className="align-self-end">
+                        <button
+                            type="button"
+                            className="button button-pink button-min-width mr-1">
+                            cancel
+                        </button>
+                        <button
+                            className="button button-light-blue button-min-width">
+                            add 
+                        </button>
+                    </div>
+                </form>
             </Modal>
         </div>
     );
